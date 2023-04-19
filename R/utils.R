40d33e508d67c0947dd0b5a13f7e3e9a9dca1cd5
Carl Suster <Carl.Suster@health.nsw.gov.au>
2023-04-19 12:21:48 +1000

    Register postcode aliases with cartographer

--- R/utils.R
+++ R/utils.R
@@ -31,15 +31,20 @@ normalise_state_names <- function(names) {
 #'
 #' Some special postcodes are used in addresses, such as codes for post office boxes.
 #' This helper converts those to the postcode for the closest normal suburb if there
-#' is a reasonable clear match.
+#' is a reasonable clear match. If there is no good match, the postcodes are left
+#' unchanged.
 #'
-#' If there is no good match, the postcodes are left unchanged.
+#' Note that this goes a little further than the aliases that are registered with
+#' cartographer (which only account for postcodes with no geospatial data in the ABS
+#' dataset).
 #'
 #' @param codes Character vector of postcodes (or coercible to one).
 #'
 #' @return Character vector of the same size as the input, but with the normalised postcodes.
 #'
 #' @export
+#' @examples
+#' normalise_postcodes(c(1685, 2000, 1010, 2129, 2145))
 normalise_postcodes <- function(codes) {
   codes <- as.character(codes)
   dict <- stats::setNames(nswgeo::postcodes$canonical, nswgeo::postcodes$postcode)
--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,12 @@
 .onLoad <- function(libname, pkgname) {
+  pcode_aliases <- {
+    aliases <- (nswgeo::postcodes$postcode != nswgeo::postcodes$canonical) &
+               (! nswgeo::postcodes$postcode %in% nswgeo::poa_nsw$POA_CODE_2021) &
+               (nswgeo::postcodes$canonical %in% nswgeo::poa_nsw$POA_CODE_2021)
+    setNames(nswgeo::postcodes$postcode[aliases],
+             nswgeo::postcodes$canonical[aliases])
+  }
+
   cartographer::register_map("nswgeo.lhd", nswgeo::lhd,
                           feature_column = "lhd_name",
                           outline = nswgeo::nsw)
@@ -7,7 +15,8 @@
                           outline = nswgeo::nsw)
   cartographer::register_map("nswgeo.postcode", nswgeo::poa_nsw,
                           feature_column = "POA_CODE_2021",
-                          outline = nswgeo::nsw)
+                          outline = nswgeo::nsw,
+                          aliases = pcode_aliases)
   cartographer::register_map("nswgeo.states", nswgeo::states,
                           feature_column = "STE_NAME21",
                           outline = nswgeo::australia,
