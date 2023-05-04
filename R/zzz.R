f432198ab9727a1749047eae64a03cca7662b39f
Carl Suster <Carl.Suster@health.nsw.gov.au>
2023-05-04 16:53:17 +1000

    Compute postcode aliases lazily

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-  pcode_aliases <- {
-    aliases <- (nswgeo::postcodes$postcode != nswgeo::postcodes$canonical) &
-               (! nswgeo::postcodes$postcode %in% nswgeo::poa_nsw$POA_CODE_2021) &
-               (nswgeo::postcodes$canonical %in% nswgeo::poa_nsw$POA_CODE_2021)
-    stats::setNames(nswgeo::postcodes$postcode[aliases],
-                    nswgeo::postcodes$canonical[aliases])
-  }
-
   cartographer::register_map("nswgeo.lhd", nswgeo::lhd,
                           feature_column = "lhd_name",
                           outline = sf::st_sf(geometry = lhd_outline))
@@ -16,7 +8,13 @@
   cartographer::register_map("nswgeo.postcode", nswgeo::poa_nsw,
                           feature_column = "POA_CODE_2021",
                           outline = sf::st_sf(geometry = nswgeo::nsw),
-                          aliases = pcode_aliases)
+                          aliases = {
+                            aliases <- (nswgeo::postcodes$postcode != nswgeo::postcodes$canonical) &
+                              (! nswgeo::postcodes$postcode %in% nswgeo::poa_nsw$POA_CODE_2021) &
+                              (nswgeo::postcodes$canonical %in% nswgeo::poa_nsw$POA_CODE_2021)
+                            stats::setNames(nswgeo::postcodes$postcode[aliases],
+                                            nswgeo::postcodes$canonical[aliases])
+                          })
   cartographer::register_map("nswgeo.states", nswgeo::states,
                           feature_column = "STE_NAME21",
                           outline = sf::st_sf(geometry = nswgeo::australia),
